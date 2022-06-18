9cc1d96867cd5a892aeae7652592e6015b1615bf
Max <max.liao@gmail.com>
2022-06-18 19:59:23 -0400

    decompressString passes my test cases

--- decompressString/decompressString.js
+++ decompressString/decompressString.js
@@ -1,57 +1,98 @@
 // https://techdevguide.withgoogle.com/resources/former-interview-question-compression-and-decompression/#!
+const input1 = '2[3[a]b]'; // decompresses into aaabaaab
+const input2 = '2[3[a]4[b]]'; // decompresses into aaabbbbaaabbbb
+const input3 = 'a[]b';
+// 2 -> [, is number, grab rest of number (look for '['), and add to multiplier stack
+// [ -> 1, is left bracket, begin constructedStringsArr
+// ! constructedStringsArr consists of strings that need to be multiplied
+// nextInd is number, grab rest of number (look for '['), and add to multiplier stack
+// 10 -> [, nextInd is left bracket
+// [ -> a, nextInd is char, grab rest of string (look for '[' or ']'). 'a'
+// if ']', pop from multiplier stack (10), run repeat, add to constructedStringsArr. 'aaaaaaaaaa'
+// if '[', jump index to next ']' and do nothing
+// a -> ], nextInd is right bracket, begin constructedStringsArr. ['aaaaaaaaaa']
+// 'a'] -> b, nextInd is char, grab rest of string (look for '[' or ']'). 'b'
+// b -> ], nextInd is right bracket, 
+// pop from multiplier stack (2), add to constructedStringsArr. ['aaaaaaaaaa', 'b']
+// join constructedStringsArr. 'aaaaaaaaaab', run repeat. 'aaaaaaaaaabaaaaaaaaaab'
+// ] -> 0, is number, grab rest of number (look for '['), and add to multiplier stack
+// 0 -> [, nextInd is left bracket. number <= 0, skip to next ']'
+// [ -> ], do nothing
+// ] -> a, nextInd is char, grab rest of string (look for '[' or ']'). 'a'
+// a -> [, nextInd is right bracket, but current is string -> add directly to output. 'aaaaaaaaaabaaaaaaaaaaba'
+// skip to next ']'
+// ] -> b, nextInd is char, grab rest of string (look for '[' or ']'). 'b'
+// b -> end. Add directly to output. 'aaaaaaaaaabaaaaaaaaaabab'
 class DecompressString {
-    string;
     constructor(string) {
         this.string = string;
+        this.output = '';
+        this.currentIndex = 0;
+        this.currentString = '';
+        this.currentNumStr = '';
+        this.multipliers = [];
+        this.stringsToAdd = [];
         this.incrementEntry();
         console.log(this.output);
     }
-    output = '';
-    currentIndex = 0;
-    multipliers = [];
-    stringsToAdd = [];
-    buildString = '';
     isNumeric(value) {
         return /^-?\d+$/.test(value);
     }
+    isLetter(str) {
+        return str.toLowerCase() != str.toUpperCase();
+    }
+    // ! One repetition can occur inside another. For example, 2[3[a]b] decompresses into aaabaaab
+    // ! 2[4[a]3[b]] decompresses into aaaabbbaaaabbb
     incrementEntry() {
-        if (this.currentIndex >= this.string.length) {
-            return;
-        }
-        // ! One repetition can occur inside another. For example, 2[3[a]b] decompresses into aaabaaab
-        // ! 2[4[a]3[b]] decompresses into aaaabbbaaaabbb
-        const hasMultiplier = this.multipliers.length > 0;
-        const currElement = this.string[this.currentIndex];
-        if (this.isNumeric(currElement)) {
-            const newMultiplier = Number(currElement);
-            this.multipliers.push(newMultiplier);
-            // skip the '['
+        if (this.currentIndex < this.string.length) {
+            // const hasMultiplier = this.multipliers.length > 0
+            // const previousElement = this.string[this.currentIndex - 1]
+            const currElement = this.string[this.currentIndex];
+            const nextElement = this.string[this.currentIndex + 1];
+            if (this.isNumeric(currElement)) {
+                // begin constructedNumber
+                this.currentNumStr += currElement;
+                // look for rest of number(ends at '['),
+                if (nextElement === '[') {
+                    // end of number - add to multiplier stack
+                    const multiplierToAdd = Number(this.currentNumStr);
+                    console.log('multiplierToAdd:', multiplierToAdd);
+                    this.multipliers.push(multiplierToAdd);
+                    this.currentNumStr = '';
+                }
+            }
+            else if (this.isLetter(currElement)) {
+                this.currentString += currElement;
+                if (nextElement === '[') {
+                    this.currentIndex = this.string.indexOf(']', this.currentIndex);
+                }
+            }
+            else if (currElement === ']') {
+                if (this.currentString.length || this.stringsToAdd.length) {
+                    const multiplier = this.multipliers.pop();
+                    console.log('this.currentString:', this.currentString);
+                    console.log('multiplier:', multiplier);
+                    if (this.multipliers.length > 0) {
+                        this.stringsToAdd.push(this.currentString.repeat(multiplier));
+                    }
+                    else {
+                        this.stringsToAdd.push(this.currentString);
+                        this.output += [...this.stringsToAdd].join('').repeat(multiplier);
+                        this.stringsToAdd = [];
+                    }
+                    console.log('this.stringsToAdd:', this.stringsToAdd);
+                    this.currentString = '';
+                }
+            }
             this.currentIndex++;
-        }
-        else if (currElement === ']') {
-            if (this.buildString.length > 0 && hasMultiplier) {
-                const multiplier = this.multipliers.pop();
-                this.stringsToAdd.push(this.buildString.repeat(multiplier));
-                this.buildString = '';
-            }
-            else if (this.stringsToAdd.length) {
-                const multiplier = this.multipliers.pop();
-                this.output += [...this.stringsToAdd].join('').repeat(multiplier);
-                this.stringsToAdd = [];
-            }
-        }
-        else {
-            // ! currElement is letter
-            // check if we just entered a bracket
-            if (this.string[this.currentIndex - 1] === '[') {
-                this.buildString = this.getSubstring();
+            if (this.currentIndex < this.string.length) {
+                this.incrementEntry();
             }
             else {
-                this.stringsToAdd.push(currElement, this.buildString);
+                this.output += this.currentString;
             }
+            return;
         }
-        this.currentIndex++;
-        this.incrementEntry();
     }
     getSubstring() {
         const endInd = this.string.indexOf(']', this.currentIndex);
@@ -59,12 +100,8 @@ class DecompressString {
         return this.string.substring(this.currentIndex, endInd);
     }
 }
-const input1 = '2[3[a]b]'; // decompresses into aaabaaab
-const input2 = '2[3[a]4[b]]'; // decompresses into aaabbbbaaabbbb
-const input3 = 'a[]b';
-const input4 = '2[3[a]b]0[abc]a';
 // new DecompressString(input1)
-// new DecompressString(input2)
+new DecompressString(input2);
 // new DecompressString(input3)
-new DecompressString(input4);
-// console.log('a'.repeat(0))
+const input4 = '2[10[a]b]0[abc]a[]b';
+// new DecompressString(input4)
